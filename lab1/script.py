bbfcb82221084fbe1d24a82a55d1125bc02acb04
Гасан Нарар 21КІ <Nazarhasan327@gmail.com>
2026-09-08 19:50:07 +0300

    lab 1 done

---
+++ lab1/script.py
@@ -0,0 +1,48 @@
+
+print('Hello World')
+
+a = int(2)
+b = len('Hello World')
+c = float(2.5)
+d = ["a", "b", "c", "d"]
+print(d)
+e = dict(a=1, b=2, c=3)
+f = str('Hello World')
+g = bool(True)
+h = (1, 2, 3)
+print(h)
+i = {1, 2, 3, 4}
+print(i)
+
+a = 10
+b = 2
+print(a + b)
+
+a = 12
+b = 3
+print(a - b)
+
+
+a = 2
+b = 4
+print(a * b)
+
+
+a = 3
+b = 4
+print(a ** b)
+
+
+a = 6
+b = 2
+print(a / b)
+
+
+a = 8
+b = 4
+print(a // b)
+
+
+a = 8
+b = 2
+print(a % b)
